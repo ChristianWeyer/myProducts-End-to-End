34714713f6cbfb00928cecf44172e60d35eb9fd0
Christian Weyer <christian.weyer@thinktecture.com>
2013-08-21 21:26:40 +0200

    Completed translations

--- MasterDetail.Web/app/translations/translations-de.js
+++ MasterDetail.Web/app/translations/translations-de.js
@@ -1,18 +1,23 @@
 ﻿var tt = window.tt || {}; tt.translations = {};
 tt.translations.de = {
-    "LOGIN_LOGIN": "Anmeldung",
+    "LOGIN_TITLE": "Anmeldung",
     "LOGIN_USERNAME": "Benutzername",
     "LOGIN_PASSWORD": "Passwort",
     "LOGIN_BUTTON_LOGIN": "Anmelden",
 
-    "INDEX_MYPRODUCTS": "meineProdukte",
+    "INDEX_TITLE": "meineProdukte",
     "INDEX_ARTICLES": "Artikel",
     "INDEX_INFO": "Über",
     "INDEX_USER": "Benutzerkonto",
 
-    "OVERVIEW_OVERVIEW": "Überblick",
+    "OVERVIEW_TITLE": "Überblick",
     "OVERVIEW_FILTER": "Artikel-Code-Filterung",
 
-    "ABOUT_ABOUT": "Über diese Anwendung",
-    "ABOUT_BODY": "Eine auf HTML5-basierende Produktverwaltung."
+    "DETAILS_TITLE": "Artikel-Details",
+    
+    "ABOUT_TITLE": "Über diese Anwendung",
+    "ABOUT_BODY": "Eine auf HTML5-basierende Produktverwaltung.",
+    
+    "COMMON_CANCEL": "Abbrechen",
+    "COMMON_SAVE": "Speichern"
 }
