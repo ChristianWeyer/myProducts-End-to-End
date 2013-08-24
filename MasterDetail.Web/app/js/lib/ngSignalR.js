fade7e077923932f9ffc9a8c8e05e28dbab4b23e
Christian Weyer <christian.weyer@thinktecture.com>
2013-08-24 16:01:30 +0200

    Updated ngSignalR

--- MasterDetail.Web/app/js/lib/ngSignalR.js
+++ MasterDetail.Web/app/js/lib/ngSignalR.js
@@ -1,5 +1,7 @@
-﻿angular.module('ngSignalR', ['ng'])
-    .factory('hubProxy', ['$rootScope', function ($rootScope) {
+﻿'use strict';
+
+angular.module('ngSignalR', ['ng']).
+    factory('hubProxy', ['$rootScope', function ($rootScope) {
         function signalRHubProxyFactory(serverUrl, hubName) {
             var connection = $.hubConnection(serverUrl);
             connection.logging = true;
@@ -16,31 +18,39 @@
                     proxy = null;
                 },
                 on: function (eventName, callback) {
-                    proxy.on(eventName, function (result) {
-                        $rootScope.$apply(function () {
-                            if (callback) {
-                                callback(result);
-                            }
-                        });
+                    proxy.on(eventName, function () {
+                        var eventNameArguments = arguments;
+                        if (callback) {
+                            $rootScope.$apply(function () {
+                                callback.apply(callback, eventNameArguments);
+                            });
+                        }
                     });
                 },
                 off: function (eventName, callback) {
-                    proxy.off(eventName, function (result) {
-                        $rootScope.$apply(function () {
-                            if (callback) {
-                                callback(result);
-                            }
-                        });
+                    proxy.off(eventName, function () {
+                        var eventNameArguments = arguments;
+                        if (callback) {
+                            $rootScope.$apply(function () {
+                                callback.apply(callback, eventNameArguments);
+                            });
+                        }
                     });
                 },
-                invoke: function (methodName, callback) {
-                    proxy.invoke(methodName, arguments[1])
+                invoke: function () { // params methodName, argsForMethodName..., callback
+                    var len = arguments.length;
+                    var args = Array.prototype.slice.call(arguments); // convert to real array
+                    var callback = undefined;
+                    if (len > 1) {
+                        callback = args.pop();
+                    }
+                    proxy.invoke.apply(proxy, args)
                         .done(function (result) {
-                            $rootScope.$apply(function () {
-                                if (callback && result) {
+                            if (callback) {
+                                $rootScope.$apply(function () {
                                     callback(result);
-                                }
-                            });
+                                });
+                            }
                         });
                 },
                 connection: connection
