e2deb9a789617cdc11dbbb1e2578e45d75db6f7b
Christian Weyer <christian.weyer@thinktecture.com>
2013-09-15 22:34:49 +0200

    API controller for image upload

---
+++ MasterDetail.Web/Api/Controllers/ImageUploadController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.Http;
+
+namespace MasterDetail.Web.Api.Controllers
+{
+    public class ImageUploadController : ApiController
+    {
+        private static readonly string mediaPath = HttpContext.Current.Server.MapPath("~/Images/");
+        private static readonly string applicationPath = HostingEnvironment.MapPath("~/");
+
+        public async Task<IEnumerable<string>> Post()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            var multipartStreamProvider = new MultipartFormDataStreamProvider(mediaPath);
+            await Request.Content.ReadAsMultipartAsync(multipartStreamProvider);
+
+            return multipartStreamProvider.FileData.Select(fd => ResolveVirtual(fd.LocalFileName));
+        }
+
+
+        private static string ResolveVirtual(string physicalPath)
+        {
+            var url = physicalPath.Substring(applicationPath.Length).Replace('\\', '/');
+
+            return (url);
+        }
+    }
+}
