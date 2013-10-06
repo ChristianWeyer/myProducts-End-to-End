3b8ecde6276a4fe054f0aa82ba5cd39ca31fe52a
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-06 20:42:51 +0200

    Scrolling

--- src/MasterDetail.Web/app/js/app.js
+++ src/MasterDetail.Web/app/js/app.js
@@ -1,5 +1,5 @@
 ﻿define(['services/routeResolver'], function () {
-    var app = angular.module("myApp", ["ngRoute", "ngTouch", "ngAnimate", "$strap.directives", "ui.bootstrap", "kendo.directives", "tt.SignalR", "tt.Authentication", "ngCookies", "pascalprecht.translate", "routeResolverServices"]);
+    var app = angular.module("myApp", ["ngRoute", "ngTouch", "ngAnimate", "$strap.directives", "ui.bootstrap", "kendo.directives", "tt.SignalR", "tt.Authentication", "ngCookies", "pascalprecht.translate", "routeResolverServices", "ng-scrollable"]);
 
     app.config(["$routeProvider", "$locationProvider", "$translateProvider", "$httpProvider", "routeResolverProvider", "$controllerProvider", "$compileProvider", "$filterProvider", "$provide",
         function ($routeProvider, $locationProvider, $translateProvider, $httpProvider, routeResolverProvider, $controllerProvider, $compileProvider, $filterProvider, $provide) {
