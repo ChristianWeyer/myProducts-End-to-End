a639ebd070090c5a3d3f62723c497b5d62166eb9
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-11 11:24:32 +0200

    Adding a repro for carousel issue

---
+++ src/MasterDetail.Web/repro/app.js
@@ -0,0 +1,16 @@
+﻿var app = angular.module("app", ["angular-carousel"]);
+
+app.controller("GalleryController", ["$scope", "$http", function ($scope, $http) {
+
+    $scope.loadImages = function () {
+        $http({
+            method: "GET",
+            url: "../api/images",
+            cache: true
+        }).then(function (response) {
+            $scope.productImages = response.data;
+        });
+    };
+
+    $scope.loadImages();
+}]);
