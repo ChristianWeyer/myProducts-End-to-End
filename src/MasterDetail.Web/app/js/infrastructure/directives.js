1957742319295c4049d6d280d5dc726b48816c42
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-12 15:23:33 +0200

    More fluid

--- src/MasterDetail.Web/app/js/infrastructure/directives.js
+++ src/MasterDetail.Web/app/js/infrastructure/directives.js
@@ -1,14 +1,28 @@
 ﻿define(["app"], function (app) {
-    app.directive('resetSourceWhen', function () {
-        return function(scope, element, attrs) {
-            var ds = element.inheritedData('$kendoDataSource');
+    app.directive("resetSourceWhen", function () {
+        return function (scope, element, attrs) {
+            var ds = element.inheritedData("$kendoDataSource");
+            
             if (ds) {
-                scope.$watch(attrs.resetSourceWhen, function(newVal, oldVal) {
+                scope.$watch(attrs.resetSourceWhen, function (newVal, oldVal) {
                     if (newVal !== oldVal && newVal) {
                         ds.filter(null);
                     }
                 });
             }
+        };
+    });
+    
+    app.directive("chartAutoResize", function () {
+        return function (scope, element, attrs) {
+            $(window).resize(function () {
+                var chartDiv = element;
+                var chart = chartDiv.data("kendoChart");
+                chart.options.transitions = false;
+                chartDiv.css({ display: "none" });
+                chartDiv.css({ width: chartDiv.parent().innerWidth(), display: "block" });
+                chart.redraw();
+            });
         };
     });
 });
