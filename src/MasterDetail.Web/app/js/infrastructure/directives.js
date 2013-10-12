f4d7ae8c9d3709b2a9af374be9a6c1e7ac3b6b67
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-12 15:49:13 +0200

    UI fixes

--- src/MasterDetail.Web/app/js/infrastructure/directives.js
+++ src/MasterDetail.Web/app/js/infrastructure/directives.js
@@ -2,7 +2,7 @@
     app.directive("resetSourceWhen", function () {
         return function (scope, element, attrs) {
             var ds = element.inheritedData("$kendoDataSource");
-            
+
             if (ds) {
                 scope.$watch(attrs.resetSourceWhen, function (newVal, oldVal) {
                     if (newVal !== oldVal && newVal) {
@@ -12,16 +12,19 @@
             }
         };
     });
-    
+
     app.directive("chartAutoResize", function () {
         return function (scope, element, attrs) {
             $(window).resize(function () {
                 var chartDiv = element;
                 var chart = chartDiv.data("kendoChart");
-                chart.options.transitions = false;
-                chartDiv.css({ display: "none" });
-                chartDiv.css({ width: chartDiv.parent().innerWidth(), display: "block" });
-                chart.redraw();
+                
+                if (chart) {
+                    chart.options.transitions = false;
+                    chartDiv.css({ display: "none" });
+                    chartDiv.css({ width: chartDiv.parent().innerWidth(), display: "block" });
+                    chart.redraw();
+                }
             });
         };
     });
