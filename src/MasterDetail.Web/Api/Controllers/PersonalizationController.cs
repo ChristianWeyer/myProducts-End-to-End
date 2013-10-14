52f35fde5ef14d6776ad38363c62d96dda0bbe20
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-14 14:15:08 +0200

    Updated index.html

--- src/MasterDetail.Web/Api/Controllers/PersonalizationController.cs
+++ src/MasterDetail.Web/Api/Controllers/PersonalizationController.cs
@@ -2,7 +2,6 @@
 using System.Linq;
 using System.Security.Principal;
 using System.Web.Http;
-using MasterDetail.Web.Api.DTOs;
 using MasterDetail.Web.Api.DTOs.Personalization;
 
 namespace MasterDetail.Web.Api.Controllers
