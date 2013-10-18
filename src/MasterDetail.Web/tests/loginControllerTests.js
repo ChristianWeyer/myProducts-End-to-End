c4efb1b295cb75c3a377d7b19c48ef5310fdfcba
Christian Weyer <christian.weyer@thinktecture.com>
2013-10-18 16:22:58 +0200

    Still working on basic tests

--- src/MasterDetail.Web/tests/loginControllerTests.js
+++ src/MasterDetail.Web/tests/loginControllerTests.js
@@ -1,10 +1,35 @@
-﻿/// <reference path="../app/js/lib/require.js" />
-/// <reference path="jasmine.js" />
-/// <reference path="../main.js" />
+﻿/// <reference path="jasmine.js" />
+/// <reference path="../app/js/lib/angular-.js" />
+/// <reference path="angular-mocks.js" />
+/// 
+/// <reference path="../app/js/lib/" />
+/// <reference path="../app/js/infrastructure/" />
+/// <reference path="../app/js/services/" />
+/// <reference path="../app/js/constants.js" />
+/// <reference path="../app/translations/translations-de.js" />
+/// <reference path="../app/js/app.js" />
 /// <reference path="../app/js/controllers/loginController.js" />
 
-describe("Login controller", function() {
-    it("foo", function() {
+describe("Login controller", function () {
+    var controller;
+    var scope;
+    var _tokenAuthentication;
+    
+    beforeEach(module("myApp"));
+
+    beforeEach(inject(function ($rootScope, $controller, tokenAuthentication) {
+        scope = $rootScope.$new();
+        
+        controller = $controller(LoginController, {
+            $scope: scope, _tokenAuthentication: tokenAuthentication
+        });
+    }));
+    
+    it("Should have a LoginController controller", function () {
+        expect(app.LoginCtrl).not.to.equal(null);
+    });
+
+    it("Correct user can log in", function () {
 
     });
 })
