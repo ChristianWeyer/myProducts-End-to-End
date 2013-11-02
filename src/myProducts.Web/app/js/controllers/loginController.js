8860aa5b34dbbc74c8b4ab30a9f8fe2aea62d073
Christian Weyer <christian.weyer@thinktecture.com>
2013-11-02 15:35:31 +0100

    Improved error handling

--- src/myProducts.Web/app/js/controllers/loginController.js
+++ src/myProducts.Web/app/js/controllers/loginController.js
@@ -1,11 +1,18 @@
 app.controller("LoginController",
-    ["$scope", "tokenAuthentication", function ($scope, tokenAuthentication) {
+    ["$scope", "tokenAuthentication", "toast", function ($scope, tokenAuthentication, toast) {
         $scope.login = {};
         
         $scope.login.username = "";
         $scope.login.password = "";
 
         $scope.login.submit = function () {
-            tokenAuthentication.login(ttTools.baseUrl, $scope.login.username, $scope.login.password);
+            tokenAuthentication.login(ttTools.baseUrl, $scope.login.username, $scope.login.password)
+            .error(function (data, status, headers, config) {
+                toast.pop({
+                    title: "Login",
+                    body: "Status: " + status,
+                    type: "error"
+                });
+            });
         };
     }]);
