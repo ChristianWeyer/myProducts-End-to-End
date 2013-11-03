6d2130aeca248122877cbc441ef42519239c91f5
Christian Weyer <christian.weyer@thinktecture.com>
2013-11-03 18:27:09 +0100

    Fix in login controller

--- src/myProducts.Web/app/js/controllers/loginController.js
+++ src/myProducts.Web/app/js/controllers/loginController.js
@@ -1,18 +1,20 @@
 app.controller("LoginController",
     ["$scope", "tokenAuthentication", "toast", function ($scope, tokenAuthentication, toast) {
         $scope.login = {};
-        
+
         $scope.login.username = "";
         $scope.login.password = "";
 
         $scope.login.submit = function () {
             tokenAuthentication.login(ttTools.baseUrl, $scope.login.username, $scope.login.password)
             .error(function (data, status, headers, config) {
-                toast.pop({
-                    title: "Login",
-                    body: "Status: " + status,
-                    type: "error"
-                });
+                if (status !== 401) {
+                    toast.pop({
+                        title: "Login",
+                        body: "Status: " + status,
+                        type: "error"
+                    })
+                };
             });
         };
     }]);
