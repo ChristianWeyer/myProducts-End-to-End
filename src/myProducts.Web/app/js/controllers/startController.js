0da9898252252416ad2386c2cb75bc49844a08be
Christian Weyer <christian.weyer@thinktecture.com>
2013-11-03 22:57:51 +0100

    Small fix

--- src/myProducts.Web/app/js/controllers/startController.js
+++ src/myProducts.Web/app/js/controllers/startController.js
@@ -1,7 +1,18 @@
 app.controller("StartController", ["$scope", "personalization", function ($scope, personalization) {
     $scope.start = {};
-    $scope.start.navigationItems = personalization.data.Features.filter(function (value, index) {
-        return value.DisplayText;
-    });
     $scope.start.classes = ["", "'bg-color-blue'", "'bg-color-blueDark'"];
+
+    if (personalization.data) {
+        $scope.start.navigationItems = getTileItems();
+    }
+    
+    $scope.$on(tt.personalization.dataLoaded, function () {
+        $scope.start.navigationItems = getTileItems();
+    });
+
+    function getTileItems() {
+        return personalization.data.Features.filter(function (value, index) {
+            return value.DisplayText;
+        });
+    }
 }]);
