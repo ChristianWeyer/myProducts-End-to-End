19914f9f7a201551ffc6a704cbfc245b66e403f0
Christian Weyer <christian.weyer@thinktecture.com>
2013-11-18 19:38:14 +0100

    Removed PDF export settings

--- src/myProducts.Web/app/js/controllers/settingsController.js
+++ src/myProducts.Web/app/js/controllers/settingsController.js
@@ -1,7 +1,6 @@
     app.controller("SettingsController", ["$scope", "$rootScope", "settings", function ($scope, $rootScope, settings) {
         $scope.settings = {};
         $scope.settings.enablePush = settings.enablePush;
-        $scope.settings.enablePdfExport = settings.enablePdfExport;
         
         angular.forEach($scope.settings, function (value, key) {
             $scope.$watch("settings." + key, function (newVal, oldVal) {
