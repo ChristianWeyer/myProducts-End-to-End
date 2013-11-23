73c6363a17056e1625e5525bccd141cdc8e93da6
Christian Weyer <christian.weyer@thinktecture.com>
2013-11-23 18:02:31 +0100

    Cleanup

--- src/myProducts.DataAccess/DbContextExtensions.cs
+++ src/myProducts.DataAccess/DbContextExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.Core.Objects;
 using System.Linq;
 
 namespace MyProducts.Model
@@ -22,6 +24,41 @@ namespace MyProducts.Model
             {
                 db.Entry(entity).State = EntityState.Modified;
                 notToBeModified.ToList().ForEach(e => db.Entry(entity).Property(e).IsModified = false);
+            }
+        }
+
+        public static void AttachByIdValue<TEntity>(this DbContext context, TEntity rootEntity, HashSet<Type> childTypes)
+            where TEntity : EntityBase
+        {
+            context.Set<TEntity>().Add(rootEntity);
+
+            if (rootEntity.Id != Guid.Empty)
+            {
+                context.Entry(rootEntity).State = EntityState.Modified;
+            }
+
+            foreach (var entry in context.ChangeTracker.Entries<EntityBase>())
+            {
+                if (entry.State == EntityState.Added && entry.Entity != rootEntity)
+                {
+                    if (childTypes == null || childTypes.Count == 0)
+                    {
+                        entry.State = EntityState.Unchanged;
+                    }
+                    else
+                    {
+                        var entityType = ObjectContext.GetObjectType(entry.Entity.GetType());
+
+                        if (!childTypes.Contains(entityType))
+                        {
+                            entry.State = EntityState.Unchanged;
+                        }
+                        else if (entry.Entity.Id != Guid.Empty)
+                        {
+                            entry.State = EntityState.Modified;
+                        }
+                    }
+                }
             }
         }
     }
--- src/tools/build_index.html.bat
+++ src/tools/build_index.html.bat
@@ -3,3 +3,5 @@ curl -k https://localhost/ngmd/ > index.html
 ssr -f index.html -a -s "/ngmd/" -r "/"
 
 move index.html ..\myProducts.SelfHost\client
+
+del *.bak
