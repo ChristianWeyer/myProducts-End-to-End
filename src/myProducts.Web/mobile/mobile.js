85b60f34dc4cf7ba90c530a8b8e3a841310be249
Christian Weyer <christian.weyer@thinktecture.com>
2013-12-01 12:12:23 +0100

    Prepare for PG

---
+++ src/myProducts.Web/mobile/mobile.js
@@ -0,0 +1,23 @@
+﻿var mobileApp = {
+    initialize: function () {
+        if(!document.URL.match(/^https?:/)){
+            this.bindEvents();
+        }
+    },
+
+    bindEvents: function () {
+        document.addEventListener("load", this.onLoad, false);
+        document.addEventListener("deviceready", this.onDeviceReady, false);
+        window.addEventListener("orientationchange", orientationChange, true);
+    },
+
+    onLoad: function () {
+    },
+
+    onDeviceReady: function () {
+        //alert("onDeviceReady");
+        angular.element(document).ready(function () {
+            StatusBar.overlaysWebView(false);
+        });
+    }
+};
