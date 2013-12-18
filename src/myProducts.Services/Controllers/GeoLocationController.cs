a08de9102c794c801112674058d90ea6a5d8e3ff
Christian Weyer <christian.weyer@thinktecture.com>
2013-12-18 22:35:12 +0100

    GeoLocation API controller

---
+++ src/myProducts.Services/Controllers/GeoLocationController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace MyProducts.Services.Controllers
+{
+    [AllowAnonymous]
+    public class GeoLocationController : ApiController
+    {
+        public void Post(dynamic loc)
+        {
+            
+        }
+    }
+}
