90f8b852b144aab57ac6bd51a8899e49551b6027
Christian Weyer <christian.weyer@thinktecture.com>
2013-12-30 23:22:16 +0100

    Changes in local deployment

--- src/tools/build_index.html.bat
+++ src/tools/build_index.html.bat
@@ -2,7 +2,7 @@ curl -k https://localhost/ngmd/app > index.html
 
 ssr -f index.html -a -s "/ngmd/" -r ""
 
-copy index.html ..\myProducts.WindowsClient\client
-move index.html ..\myProducts.SelfHost\client
+copy index.html ..\build\client
+move index.html ..\CEFSharp\client
 
 del *.bak
