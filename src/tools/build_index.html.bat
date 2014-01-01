611f8aa1a2336be10a6058c856635785b7c43adb
Christian Weyer <christian.weyer@thinktecture.com>
2014-01-01 20:12:31 +0100

    CEFSharp client tweaks

--- src/tools/build_index.html.bat
+++ src/tools/build_index.html.bat
@@ -3,6 +3,6 @@ curl -k https://localhost/ngmd/app > index.html
 ssr -f index.html -a -s "/ngmd/" -r ""
 
 copy index.html ..\build\client
-move index.html ..\CEFSharp\client
+move index.html ..\ClientBuild\client
 
 del *.bak
