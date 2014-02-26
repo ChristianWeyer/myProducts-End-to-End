d22f4e5108213480c55cf8ad485ff7f2924b66af
Christian Weyer <christian.weyer@thinktecture.com>
2014-02-26 13:58:34 +0100

    Improved warump tool

--- src/myProducts.WarmupHelper/Program.cs
+++ src/myProducts.WarmupHelper/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Threading;
+using System.Threading.Tasks;
+using Thinktecture.IdentityModel.Client;
 
 namespace myProducts.WarmupHelper
 {
@@ -8,29 +12,66 @@ namespace myProducts.WarmupHelper
     {
         static void Main()
         {
-            var localClient = new HttpClient { BaseAddress = new Uri("https://windows8vm/ngmd/api/") };
-            localClient.SetBasicAuthentication("cw", "cw");
+            ServicePointManager.DefaultConnectionLimit = Int32.MaxValue;
+            // TODO: no, not really! :(
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
 
-            var cloudClient = new HttpClient { BaseAddress = new Uri("https://demo.christianweyer.net/api/") };
-            cloudClient.SetBasicAuthentication("cw", "cw");
+            CallServices();
+            Console.ReadLine();
+        }
 
+        private static async void CallServices()
+        {
+            var localClient = new HttpClient { BaseAddress = new Uri("https://windows8vm/ngmd/") };
+            var localToken = GetToken("https://windows8vm/ngmd/token");
+            localClient.SetBearerToken(localToken.AccessToken);
+
+            var cloudClient = new HttpClient { BaseAddress = new Uri("https://demo.christianweyer.net/") };
+            var cloudToken = GetToken("https://demo.christianweyer.net/token");
+            cloudClient.SetBearerToken(cloudToken.AccessToken);
+
+            var tasks = new List<Task>();
             while (true)
             {
-                Console.WriteLine("Calling 'personalization'");
-                localClient.GetAsync("personalization");
-                cloudClient.GetAsync("personalization");
-                Console.WriteLine("Calling 'articles'");
-                localClient.GetAsync("articles");
-                cloudClient.GetAsync("articles");
-                Console.WriteLine("Calling 'images'");
-                localClient.GetAsync("images");
-                cloudClient.GetAsync("images");
-                Console.WriteLine("Calling 'statistics'");
-                localClient.GetAsync("statistics");
-                cloudClient.GetAsync("statistics");
+                try
+                {
+                    Console.WriteLine("Calling 'app'");
+                    tasks.Add(localClient.GetAsync("app"));
+                    tasks.Add(cloudClient.GetAsync("app"));
 
-                Thread.Sleep(10000);
+                    Console.WriteLine("Calling 'personalization'");
+                    tasks.Add(localClient.GetAsync("api/personalization"));
+                    tasks.Add(cloudClient.GetAsync("api/personalization"));
+
+                    Console.WriteLine("Calling 'articles'");
+                    tasks.Add(localClient.GetAsync("api/articles"));
+                    tasks.Add(cloudClient.GetAsync("api/articles"));
+
+                    Console.WriteLine("Calling 'images'");
+                    tasks.Add(localClient.GetAsync("api/images"));
+                    tasks.Add(cloudClient.GetAsync("api/images"));
+
+                    Console.WriteLine("Calling 'statistics'");
+                    tasks.Add(localClient.GetAsync("api/statistics/distribution"));
+                    tasks.Add(cloudClient.GetAsync("api/statistics/distribution"));
+
+                    await Task.WhenAll(tasks);
+                    await Task.Delay(10000);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("EXCEPTION: " + ex.Message);
+                    //throw;
+                }
             }
+        }
+
+        private static TokenResponse GetToken(string tokenUrl)
+        {
+            var client = new OAuth2Client(
+                new Uri(tokenUrl));
+
+            return client.RequestResourceOwnerPasswordAsync("cw", "cw").Result;
         }
     }
 }
