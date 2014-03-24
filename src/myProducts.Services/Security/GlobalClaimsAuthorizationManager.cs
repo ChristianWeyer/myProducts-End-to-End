e210bddd9fdf42a2ccadece4488170859e16c6bf
Christian Weyer <christian.weyer@thinktecture.com>
2014-03-24 19:29:55 +0100

    A little more cleanup

--- src/myProducts.Services/Security/GlobalClaimsAuthorizationManager.cs
+++ src/myProducts.Services/Security/GlobalClaimsAuthorizationManager.cs
@@ -7,24 +7,18 @@ namespace MyProducts.Services.Security
     {
         public override bool CheckAccess(AuthorizationContext context)
         {
-            if (ClaimsPrincipal.Current.Identity.IsAuthenticated)
+            if (!ClaimsPrincipal.Current.Identity.IsAuthenticated) return false;
+            
+            if (!ClaimsPrincipal.Current.HasClaim(ApplicationClaimTypes.Default, ApplicationClaimsValues.Present)) return false;
+
+            if (context.Action.FirstOrDefault(ac => ac.Value == "Save") != null &&
+                context.Resource.FirstOrDefault(rc => rc.Value == "Article") != null &&
+                !ClaimsPrincipal.Current.HasClaim(ApplicationClaimTypes.Maintenance, ApplicationClaimsValues.Editor))
             {
-                if (!ClaimsPrincipal.Current.HasClaim(ApplicationClaimTypes.Default, ApplicationClaimsValues.Present))
-                {
-                    return false;
-                }
-
-                if (context.Action.FirstOrDefault(ac => ac.Value == "Save") != null &&
-                    context.Resource.FirstOrDefault(rc => rc.Value == "Article") != null &&
-                    !ClaimsPrincipal.Current.HasClaim(ApplicationClaimTypes.Maintenance, ApplicationClaimsValues.Editor))
-                {
-                    return false;
-                }
-
-                return true;
+                return false;
             }
 
-            return false;
+            return true;
         }
     }
 }
