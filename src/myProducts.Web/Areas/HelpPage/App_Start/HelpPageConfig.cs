b878b0b7cd44ac7d49c79161a5d2e7e3a6b6671e
Christian Weyer <christian.weyer@thinktecture.com>
2014-04-25 13:53:49 +0200

    Mobile fix

--- src/myProducts.Web/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ src/myProducts.Web/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -1,13 +1,11 @@
 // Uncomment the following to provide samples for PageResult<T>. Must also add the Microsoft.AspNet.WebApi.OData
 // package to your project.
-////#define Handle_PageResultOfT
+#define Handle_PageResultOfT
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Net.Http.Headers;
 using System.Reflection;
 using System.Web;
@@ -34,7 +32,7 @@ namespace MyProducts.Web.Areas.HelpPage
         public static void Register(HttpConfiguration config)
         {
             //// Uncomment the following to use the documentation from XML documentation file.
-            //config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/XmlDocument.xml")));
+            config.SetDocumentationProvider(new XmlDocumentationProvider(HttpContext.Current.Server.MapPath("~/App_Data/MyProducts.Services.XML")));
 
             //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
             //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
