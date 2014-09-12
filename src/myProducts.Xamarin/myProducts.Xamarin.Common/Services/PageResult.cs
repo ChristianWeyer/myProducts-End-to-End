95a409bf1253a31d108864f31da752be9c7f561b
Christian Weyer <christian.weyer@thinktecture.com>
2014-09-12 12:43:09 +0200

    Re-added correct de-ser. code

--- src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/PageResult.cs
+++ src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/PageResult.cs
@@ -6,17 +6,17 @@ using Newtonsoft.Json;
 
 namespace myProducts.Xamarin.Common.Services
 {
-    //[JsonObject]
-    //[DataContract]
+    [JsonObject]
+    [DataContract]
 	public class PageResult<T> : IEnumerable<T>, IEnumerable
 	{
-        //[DataMember]
+        [DataMember]
 		public IEnumerable<T> Items { get; set; }
 
-        //[DataMember]
+        [DataMember]
 		public long? Count { get; set; }
 
-        //[DataMember]
+        [DataMember]
 		public Uri NextPageLink { get; set; }
 
 		public PageResult(IEnumerable<T> items, Uri nextPageLink, long? count)
