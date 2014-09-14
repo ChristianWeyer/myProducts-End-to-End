2bacf726758bc597f6a897682e13746e4e3709ac
Manuel Rauber <github@manuel-rauber.de>
2014-09-14 23:19:50 +0200

    Added StatisticsServiceClient to get statistics from Web API

---
+++ src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/StatisticsServiceClient.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyProducts.Services.DTOs;
+using myProducts.Xamarin.Contracts.Services;
+
+namespace myProducts.Xamarin.Common.Services
+{
+	public class StatisticsServiceClient : BaseServiceClient, IStatisticsServiceClient
+	{
+		private const string DistributionUrlTemplate = "statistics/distributin";
+		private const string SalesUrlTemplate = "statistics/sales";
+
+		public StatisticsServiceClient(ITokenManager tokenManager) : base(tokenManager) {}
+		
+		public async Task<IEnumerable<DistributionDto>> GetDistribution()
+		{
+			var response = await Get<IEnumerable<DistributionDto>>(DistributionUrlTemplate);
+			return response;
+		}
+
+		public async Task<IEnumerable<SalesDto>> GetSales()
+		{
+			var response = await Get<IEnumerable<SalesDto>>(SalesUrlTemplate);
+			return response;
+		}
+	}
+}
---
+++ src/myProducts.Xamarin/myProducts.Xamarin.Contracts/Services/IStatisticsServiceClient.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using MyProducts.Services.DTOs;
+
+namespace myProducts.Xamarin.Contracts.Services
+{
+	public interface IStatisticsServiceClient
+	{
+		Task<IEnumerable<DistributionDto>> GetDistribution();
+		Task<IEnumerable<SalesDto>> GetSales();
+	}
+}
