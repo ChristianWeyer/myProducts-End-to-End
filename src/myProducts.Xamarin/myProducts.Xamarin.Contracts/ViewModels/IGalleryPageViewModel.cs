f6fd3d5f9a0eb0fa4ad86c12e0da9b324ef3e220
Manuel Rauber <github@manuel-rauber.de>
2014-09-17 18:42:59 +0200

    Added ViewModel for GalleryPage

---
+++ src/myProducts.Xamarin/myProducts.Xamarin.Contracts/ViewModels/IGalleryPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+
+namespace myProducts.Xamarin.Contracts.ViewModels
+{
+	public interface IGalleryPageViewModel : IBusyIndicator
+	{
+		ObservableCollection<string> Images { get; }
+		Task DownloadImages();
+	}
+}
---
+++ src/myProducts.Xamarin/myProducts.Xamarin.ViewModels/GalleryPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using myProducts.Xamarin.Common.Extensions;
+using myProducts.Xamarin.Contracts.Services;
+using myProducts.Xamarin.Contracts.ViewModels;
+
+namespace myProducts.Xamarin.ViewModels
+{
+	public class GalleryPageViewModel : BaseViewModel, IGalleryPageViewModel
+	{
+		private readonly IGalleryServiceClient _serviceClient;
+		private ObservableCollection<string> _images;
+
+		public GalleryPageViewModel(IGalleryServiceClient serviceClient)
+		{
+			_serviceClient = serviceClient;
+		}
+
+		public ObservableCollection<string> Images
+		{
+			get { return _images ?? (_images = new ObservableCollection<string>()); }
+			private set { _images = value; }
+		}
+
+		public async Task DownloadImages()
+		{
+			IsBusy = true;
+
+			var images = await _serviceClient.GetImages();
+			
+			Images.AddRange(images);
+
+			IsBusy = false;
+		}
+	}
+}
