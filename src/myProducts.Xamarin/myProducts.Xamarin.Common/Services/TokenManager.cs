28eb5a7b906a8dea5d4db8784a4a378e2ab2301f
Manuel Rauber <github@manuel-rauber.de>
2014-09-17 20:26:28 +0200

    Updated TokenStorage to cache its token

--- src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/TokenManager.cs
+++ src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/TokenManager.cs
@@ -1,14 +1,65 @@
 ﻿using System;
 using System.Threading.Tasks;
+using myProducts.Xamarin.Contracts.IO;
 using myProducts.Xamarin.Contracts.Services;
+using Newtonsoft.Json;
 using Thinktecture.IdentityModel.Client;
 
 namespace myProducts.Xamarin.Common.Services
 {
 	public class TokenManager : ITokenManager
 	{
+		[JsonObject]
+		private class InternalTokenStorageItem
+		{
+			[JsonProperty]
+			internal string Token { get; set; }
+
+			[JsonProperty]
+			internal DateTime AbsoluteExpirationDate { get; set; }
+		}
+
 		private const string TokenUrl = "https://demo.christianweyer.net/token";
+		private const string StorageBasePath = "TokenManager";
+		private const string StorageFileName = "Token.conf";
+		private readonly IStorage _storage;
 		private string _token;
+
+		public TokenManager(IStorage storage)
+		{
+			_storage = storage;
+			_storage.BasePath = StorageBasePath;
+
+			TryReadTokenFromLocalStorage();
+		}
+
+		private void TryReadTokenFromLocalStorage()
+		{
+			var token = _storage.LoadFrom<InternalTokenStorageItem>(StorageFileName);
+
+			if (token == null)
+			{
+				return;
+			}
+
+			if (DeleteTokenIfExpired(token))
+			{
+				return;
+			}
+
+			_token = token.Token;
+		}
+
+		private bool DeleteTokenIfExpired(InternalTokenStorageItem token)
+		{
+			if (DateTime.Now > token.AbsoluteExpirationDate)
+			{
+				DeleteToken();
+				return true;
+			}
+
+			return false;
+		}
 
 		public string Token
 		{
@@ -30,10 +81,23 @@ namespace myProducts.Xamarin.Common.Services
 			}
 
 			_token = token.AccessToken;
+			SaveTokenToLocalStorage(token);
+		}
+
+		private void SaveTokenToLocalStorage(TokenResponse token)
+		{
+			var storageItem = new InternalTokenStorageItem()
+			{
+				Token = token.AccessToken,
+				AbsoluteExpirationDate = DateTime.Now.AddMilliseconds(token.ExpiresIn * 1000),
+			};
+
+			_storage.SaveTo(StorageFileName, storageItem);
 		}
 
 		public void DeleteToken()
 		{
+			_storage.Remove(StorageFileName);
 			_token = String.Empty;
 		}
 	}
