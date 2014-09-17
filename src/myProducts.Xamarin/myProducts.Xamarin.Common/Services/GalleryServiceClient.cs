6271b644929d4f6cda2b19c4a29a67899396b16a
Manuel Rauber <github@manuel-rauber.de>
2014-09-17 18:35:42 +0200

    Added Gallery Service Client

---
+++ src/myProducts.Xamarin/myProducts.Xamarin.Common/Services/GalleryServiceClient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Security;
+using System.Threading.Tasks;
+using myProducts.Xamarin.Contracts.Services;
+
+namespace myProducts.Xamarin.Common.Services
+{
+	public class GalleryServiceClient : BaseServiceClient, IGalleryServiceClient
+	{
+		private const string ImagesApiEndpoint = "images";
+
+		public GalleryServiceClient(ITokenManager tokenManager) : base(tokenManager) {}
+		
+		public async Task<IEnumerable<string>> GetImages()
+		{
+			var reponse = await Get<IEnumerable<string>>(ImagesApiEndpoint);
+			return reponse;
+		}
+	}
+}
---
+++ src/myProducts.Xamarin/myProducts.Xamarin.Contracts/Services/IGalleryServiceClient.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace myProducts.Xamarin.Contracts.Services
+{
+	public interface IGalleryServiceClient
+	{
+		Task<IEnumerable<string>> GetImages();
+	}
+}
