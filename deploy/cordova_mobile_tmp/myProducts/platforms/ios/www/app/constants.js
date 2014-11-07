fd89bfff3521723245ab0317e2435247782e4df0
Christian Weyer <christian.weyer@thinktecture.com>
2014-11-07 09:36:29 -0800

    Delete tmp folders

--- deploy/cordova_mobile_tmp/myProducts/platforms/ios/www/app/constants.js
+++
@@ -1,8 +0,0 @@
-﻿var tt = window.tt || {}; tt.personalization = tt.personalization || {}; tt.networkstatus = tt.networkstatus || {}; tt.settings = tt.settings || {};
-
-tt.personalization = {
-    dataLoaded: "tt:personalization:loaded"
-};
-tt.networkstatus = {
-    onlineChanged: "tt:networkstatus:onlinechanged"
-};
