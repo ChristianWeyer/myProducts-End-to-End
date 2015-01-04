b30c2f7311ce6fc52375082e395d64a16316bc21
Christian Weyer <christian.weyer@thinktecture.com>
2015-01-04 14:37:28 +0100

    Disable optimzations in code

---
+++ src/myProducts.Web/Gruntfile.js
@@ -0,0 +1,18 @@
+﻿module.exports = function (grunt) {
+    "use strict";
+
+    grunt.initConfig({
+        html2js: {
+            options: {
+                base: "app"
+                // custom options
+            },
+            main: {
+                src: ["app/**/*.html"],
+                dest: "app/embeddedTemplates.js"
+            },
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-html2js");
+};
