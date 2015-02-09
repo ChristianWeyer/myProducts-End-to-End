5e4ff9d6c8d6135997007b4c15d3d142a613ca24
Christian Weyer <christian.weyer@thinktecture.com>
2015-02-09 18:40:45 +0100

    Removed SQL Compact - and more clean-up

--- src/myProducts.Hosting/LoggingConfig.cs
+++ src/myProducts.Hosting/LoggingConfig.cs
@@ -1,6 +1,4 @@
 using Microsoft.AspNet.SignalR;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.Storage;
 using Serilog;
 
 namespace MyProducts.Hosting
@@ -11,17 +9,6 @@ namespace MyProducts.Hosting
         {
             var logConfig = new LoggerConfiguration()
                 .WriteTo.SignalR(GlobalHost.ConnectionManager.GetHubContext<LoggingHub>());
-
-            try
-            {
-                var setting = CloudConfigurationManager.GetSetting("StorageConnectionString");
-                var storage = CloudStorageAccount.Parse(setting);
-
-                logConfig.WriteTo.AzureTableStorage(storage);
-            }
-            catch
-            {
-            }
 
             Log.Logger = logConfig.CreateLogger();
         }
