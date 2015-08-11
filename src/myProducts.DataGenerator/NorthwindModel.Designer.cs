477f113bf2698739d881fa830ae2ff0bea3893b4
Christian Weyer <christian.weyer@thinktecture.com>
2015-08-11 18:09:57 +0200

    Revert "Update to .NET 4.6"
    This reverts commit 0b3e9d3534c15f6a67a58c3c4fb3b5a7b362c850.

--- src/myProducts.DataGenerator/NorthwindModel.Designer.cs
+++ src/myProducts.DataGenerator/NorthwindModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\chris\Documents\GitHub\myProducts-End-to-End\src\myProducts.DataGenerator\NorthwindModel.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Christian\Documents\GitHub\myProducts-End-to-End\src\myProducts.DataGenerator\NorthwindModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
